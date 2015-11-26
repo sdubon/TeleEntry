60a4587a6c9f49947b63039d61b396f51447ac0f
sdubon <dubon.sylvia@gmail.com>
2015-11-26 12:27:00 -0600

    Update rasp_i2c2.js

--- rasp_i2c2.js
+++ rasp_i2c2.js
@@ -2,46 +2,58 @@ var i2c = require('i2c');
 var address1 = 0x18;
 var TeleEntry1 = new i2c(address1, {device: '/dev/i2c-1'}); // point to your i2c address, debug provides REPL interface
 
-/*
-wire.scan(function(err, data) {
-  // result contains an array of addresses
-});
-*/
+var state = 0;
+var _ID = ""
+var _CODE = ""
+var message = ""
 
-for(var i = 0; i < 200; i++){
-TeleEntry1.writeByte("3", function(err) {});
+//"i=00001&c=9328&a=s"
+
+
+while(true){
+  TeleEntry1.readByte(function(err, res) { 
+    if (res == "!"){
+      console.log(message)
+      message = ""
+      TeleEntry1.writeByte("x", function(err) {});
+      TeleEntry1.writeByte(res, function(err) {});
+    else if (res != "#"){
+      message.concat(res)
+      TeleEntry1.writeByte("x", function(err) {});
+      TeleEntry1.writeByte(res, function(err) {});
+    }
+  console.log(res)
+  });
 }
 
-//TeleEntry1.write(["h", "o", "l", "a"], function(err) {});
-
-
-
-//TeleEntry1.writeBytes(4, ["h", "o", "l", "a"], function(err) {});
-
-for(var i = 0; i < 40; i++){
-TeleEntry1.readByte(function(err, res) { 
-// result is single byte 
-console.log(res)
-});
-}
-
 /*
-wire.readBytes(command, length, function(err, res) {
-  // result contains a buffer of bytes
-});
-
-wire.on('data', function(data) {
-  // result for continuous stream contains data buffer, address, length, timestamp
-});
-
-wire.stream(command, length, delay); // continuous stream, delay in ms
-
-
-// plain read/write
-
-//wire.write([byte0, byte1], function(err) {});
-
-//wire.read(length, function(err, res) {
-  // result contains a buffer of bytes
-});
+  switch(state){
+  case 0:
+    if (res == 'i'){      //expecting =
+      TeleEntry1.writeByte("x", function(err) {});
+      TeleEntry1.writeByte(res, function(err) {});
+      state = 10
+      break
+    }
+  case 10:
+    if (res == '='){      //expecting TeleEntry ID
+      TeleEntry1.writeByte("x", function(err) {})
+      TeleEntry1.writeByte(res, function(err) {})
+      state = 11
+      var cont = 0;
+      break
+    }
+    //code.charAt(0); 
+  case 11:
+    if (res >= '0' && res <= '9'){      //expecting TeleEntry ID
+      TeleEntry1.writeByte("x", function(err) {})
+      TeleEntry1.writeByte(res, function(err) {})
+      _ID.concat(res)  
+      cont++
+      if (cont == 5){
+        state = 20
+      }
+      
+      break
+    }
 */
